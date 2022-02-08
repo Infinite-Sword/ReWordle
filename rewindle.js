15799b4255e1883e31da567a58b125a3cb8a1380
Fisher <58302348+SwordFisherL42@users.noreply.github.com>
2022-02-08 18:39:43 -0500

    Initial commit
    Wordle with time travel between days

---
+++ rewindle.js
@@ -0,0 +1,49 @@
+function clear_local_storage(){
+    var dark_theme = false;
+    if (localStorage.getItem("darkTheme") == "true") dark_theme = true;
+    localStorage.clear();
+    if (dark_theme == true) localStorage.setItem("darkTheme", "true")
+    else localStorage.setItem("darkTheme", "false")
+}
+
+function reset_all() {
+    if (window.confirm("Are you sure? All progress will be reset.")) {
+        clear_local_storage()
+        location.reload();
+    }
+}
+
+function offset_day() {
+    clear_local_storage()
+    if (document.getElementById("day_rewind_count").value >= 1) localStorage.setItem("DateOffset", document.getElementById("day_rewind_count").value);
+    else localStorage.setItem("DateOffset", 1);
+    location.reload();
+}
+
+function current_day_setting() {
+    var date = new Date(Date.now());
+    if (localStorage.getItem("DateOffset") != null) {
+        date.setDate(date.getDate() - localStorage.getItem("DateOffset"))
+    }
+    document.getElementById("current_chrono").innerHTML = date.toDateString();;
+}
+
+function set_dark_theme() {
+    var a = document.querySelector("body");
+    if (localStorage.getItem("darkTheme") == "true") {
+        a.classList.add("nightmode");
+    }
+    else {
+        a.classList.remove("nightmode");
+    }
+}
+
+function load_custom_values() {
+    if (localStorage.getItem("DateOffset") != null) {
+        if (localStorage.getItem("DateOffset") <= 0) localStorage.setItem("DateOffset", 1);
+    }
+    else localStorage.setItem("DateOffset", 1);
+    document.getElementById("day_rewind_count").value = localStorage.getItem("DateOffset");
+    current_day_setting();
+    set_dark_theme();
+}
