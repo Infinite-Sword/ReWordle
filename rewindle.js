4e8da2bef5553d9d6fb3df62ce008234d1d678c2
Fisher <SwordFisherL42@gmail.com>
2022-02-08 19:12:44 -0500

    Fixed storage clearing function and viewport display

--- rewindle.js
+++ rewindle.js
@@ -1,20 +1,25 @@
-function clear_local_storage(){
+function clear_local_storage() {
     var dark_theme = false;
     if (localStorage.getItem("darkTheme") == "true") dark_theme = true;
     localStorage.clear();
-    if (dark_theme == true) localStorage.setItem("darkTheme", "true")
-    else localStorage.setItem("darkTheme", "false")
+    if (dark_theme == true) localStorage.setItem("darkTheme", "true");
+    else localStorage.setItem("darkTheme", "false");
 }
 
 function reset_all() {
     if (window.confirm("Are you sure? All progress will be reset.")) {
-        clear_local_storage()
+        clear_local_storage();
+        localStorage.setItem("DateOffset", 1);
         location.reload();
     }
 }
 
-function offset_day() {
-    clear_local_storage()
+function offset_day(allowed = false) {
+    if (event.key === 'Enter') {
+        allowed = true;
+    }
+    if (!allowed) return;
+    clear_local_storage();
     if (document.getElementById("day_rewind_count").value >= 1) localStorage.setItem("DateOffset", document.getElementById("day_rewind_count").value);
     else localStorage.setItem("DateOffset", 1);
     location.reload();
@@ -23,7 +28,7 @@ function offset_day() {
 function current_day_setting() {
     var date = new Date(Date.now());
     if (localStorage.getItem("DateOffset") != null) {
-        date.setDate(date.getDate() - localStorage.getItem("DateOffset"))
+        date.setDate(date.getDate() - localStorage.getItem("DateOffset"));
     }
     document.getElementById("current_chrono").innerHTML = date.toDateString();;
 }
