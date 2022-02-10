8e89c22abe26117808685f3ae21e2fcab2824424
Fisher <SwordFisherL42@gmail.com>
2022-02-10 17:00:18 -0500

    Cleanup Rewindle core functions trimmed down logic and removed type coercion

---
+++ README.md
@@ -0,0 +1,4 @@
+# [Click Here to Play Rewindle](https://swordfisherl42.github.io/Rewindle/)
+
+## Description
+This project adds controls for selecting and playing any previous "Wordle".
--- rewindle.js
+++ rewindle.js
@@ -1,53 +1,40 @@
 function clear_local_storage() {
-    var dark_theme = false;
-    if (localStorage.getItem("darkTheme") == "true") dark_theme = true;
+    var dark_theme = localStorage.getItem("darkTheme") === "true" ? true : false;
     localStorage.clear();
-    if (dark_theme == true) localStorage.setItem("darkTheme", "true");
-    else localStorage.setItem("darkTheme", "false");
+    localStorage.setItem("darkTheme", JSON.stringify(dark_theme));
 }
 
 function reset_all() {
-    if (window.confirm("Are you sure? All progress will be reset.")) {
+    if (window.confirm("Are you sure? Current progress will be reset.")) {
         clear_local_storage();
         localStorage.setItem("DateOffset", 1);
         location.reload();
     }
 }
 
-function offset_day(allowed = false) {
-    if (event.key === 'Enter') {
-        allowed = true;
-    }
-    if (!allowed) return;
+function offset_day(keypress_override = false) {
+    if (JSON.parse(localStorage.gameState).boardState[0] === "" && document.getElementById("day_rewind_count").value === localStorage.getItem("DateOffset")) return;
+    if (!keypress_override && event.key !== 'Enter') return;
+    if (JSON.parse(localStorage.gameState).boardState[0] !== "" && !window.confirm("Are you sure? Current progress will be reset.")) return;
     clear_local_storage();
-    if (document.getElementById("day_rewind_count").value >= 1) localStorage.setItem("DateOffset", document.getElementById("day_rewind_count").value);
-    else localStorage.setItem("DateOffset", 1);
+    localStorage.setItem("DateOffset", document.getElementById("day_rewind_count").value >= 1 ? document.getElementById("day_rewind_count").value : 1);
     location.reload();
 }
 
 function current_day_setting() {
     var date = new Date(Date.now());
-    if (localStorage.getItem("DateOffset") != null) {
-        date.setDate(date.getDate() - localStorage.getItem("DateOffset"));
-    }
-    document.getElementById("current_chrono").innerHTML = date.toDateString();;
+    date.setDate(date.getDate() - localStorage.getItem("DateOffset"));
+    document.getElementById("current_chrono").innerHTML = date.toDateString();
 }
 
 function set_dark_theme() {
-    var a = document.querySelector("body");
-    if (localStorage.getItem("darkTheme") == "true") {
-        a.classList.add("nightmode");
-    }
-    else {
-        a.classList.remove("nightmode");
-    }
+    var body = document.querySelector("body");
+    if (localStorage.getItem("darkTheme") === "true") body.classList.add("nightmode");
+    else body.classList.remove("nightmode");
 }
 
 function load_custom_values() {
-    if (localStorage.getItem("DateOffset") != null) {
-        if (localStorage.getItem("DateOffset") <= 0) localStorage.setItem("DateOffset", 1);
-    }
-    else localStorage.setItem("DateOffset", 1);
+    if (localStorage.getItem("DateOffset") == null || localStorage.getItem("DateOffset") <= 0) localStorage.setItem("DateOffset", 1);
     document.getElementById("day_rewind_count").value = localStorage.getItem("DateOffset");
     current_day_setting();
     set_dark_theme();
